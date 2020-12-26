96ffc650c47600af8e8e9c26864e3dc9f004f83b
Ethel Morgan <eth@ethulhu.co.uk>
2020-12-26 17:29:10 +0000

    [fish] correctly identify NixOS

--- .config/fish/functions/operating_system.fish
+++ .config/fish/functions/operating_system.fish
@@ -7,7 +7,7 @@ function operating_system --description 'Return the current Operating System'
     case Linux
       if [ -f /etc/debian_version ]
         echo 'Debian'
-      else if [ -f /nix/store ]
+      else if [ -d /nix/store ]
         echo 'NixOS'
       else
         echo 'Linux'
