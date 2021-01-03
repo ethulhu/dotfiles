62dc9680d2594c065a3d96a52ed93d28fda2c96f
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-03 20:02:29 +0000

    [fish] support Homebrew on AArch64-based macOS

--- .config/fish/config-macos.fish
+++ .config/fish/config-macos.fish
@@ -1,5 +1,10 @@
 # macOS-specific configuration.
 
+# Homebrew on AArch64 is in /opt/.
+if [ -d /opt/homebrew/bin ]
+  set -x PATH /opt/homebrew/bin $PATH
+end
+
 set -x PATH ~/.local/bin/macos $PATH
 
 set -l python_version (python3 --version | sed -E 's/.* ([[:digit:]]+\.[[:digit:]]+).*/\1/')
