49de5e4d5a4c3750b614bcd7124fb6f1ae094bc5
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-12 18:34:37 +0000

    [fish] the `make` wrapper always uses the builtin `cd`

--- .config/fish/functions/make.fish
+++ .config/fish/functions/make.fish
@@ -1,7 +1,7 @@
 function (basename (status filename) .fish) --wraps make --description 'ascend the filesystem, looking for Makefiles'
   fish --private --command "
-    while ! [ -f 'Makefile' -o -f 'makefile' -o (pwd) = $HOME -o (pwd) = '/' ]
-      cd ..
+    while ! [ -f 'Makefile' -o -f 'makefile' -o $PWD = $HOME -o $PWD = '/' ]
+      builtin cd ..
     end
 
     pwd 1>&2
