6a5eb075d3338eb35a2a8764ff85d124ae3a55e1
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-12 11:21:03 +0000

    [fish] import cd wrapper from bash

---
+++ .config/fish/functions/cd.fish
@@ -0,0 +1,9 @@
+function cd --description 'Convenience do-what-I-mean wrapper for cd' --argument-names path
+  if [ ! $path ]
+    builtin cd ~
+  else if [ -f $path ]
+    builtin cd (dirname $path)
+  else
+    builtin cd $path
+  end
+end
