92357292b68643f8af3fe76f4e04853f29cb2024
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-12 14:13:46 +0000

    [fish] make cd's builtin wrapper capable of surviving upgrades

--- .config/fish/functions/cd.fish
+++ .config/fish/functions/cd.fish
@@ -1,4 +1,14 @@
 function cd --argument-names path
+
+  # Use fish's own `cd` wrapper if possible,
+  # because it has features such as history.
+  if not functions --query __builtin_cd
+    __wrap_fish_function \
+      --name '__builtin_cd' \
+      --path $__fish_data_dir/functions/cd.fish \
+      --fallback 'builtin cd'
+  end
+
   if [ ! $path ]
     __builtin_cd ~
   else if [ -f $path ]
