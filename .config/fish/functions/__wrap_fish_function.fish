06975d32029bbd29f5bee7884ca321aaf3c1e74a
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-12 16:41:29 +0000

    [fish] various improvements to __wrap_fish_function
    Those improvements are:
    - The function is named by the filename.
    - Remove unneeded `--name=` flag for argparse.
    - Provide a more useful error message for missing flags.
    - Use `string replace` instead of `sed`.

--- .config/fish/functions/__wrap_fish_function.fish
+++ .config/fish/functions/__wrap_fish_function.fish
@@ -1,14 +1,15 @@
-function __wrap_fish_function --description 'Load a fish function from a file, giving it a new name, falling back to an alias if the function file does not exist.'
-  argparse --name __wrap_fish_function 'n/name=' 'p/path=' 'f/fallback=' -- $argv
-  or return
+function (basename (status filename) .fish) --description 'Load a fish function from a file, giving it a new name, falling back to an alias if the function file does not exist.'
+
+  argparse 'n/name=' 'p/path=' 'f/fallback=' -- $argv
+  or return 1
 
   if not set --query _flag_name _flag_path _flag_fallback
-    echo "halp"
+    echo (status function)': Missing flags!'
     return 1
   end
 
   if [ -f $_flag_path ]
-    cat $_flag_path | sed -E "s/^function [^[:space:]]+ /function $_flag_name /" | source
+    cat $_flag_path | string replace --regex '^function [^[:space:]]+ ' "function $_flag_name " | source
   else
     alias $_flag_name $_flag_fallback
   end
