dd9b363d812bd4b1ae973670759b620a2e7eb0d5
Ethel Morgan <eth@ethulhu.co.uk>
2021-01-14 18:36:19 +0000

    [fish] remove precomputes for fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -15,16 +15,21 @@ function fish_prompt --description 'Write out the prompt'
     set suffix '#'
   end
 
-  function prompt_repo
-    pwd | string replace --regex --filter "$HOME/src/([^/]*).*" ' [$1]'; or true
+  set -l prompt_user $USER
+  set -l prompt_hostname
+  if set --query SSH_CONNECTION
+    set prompt_user (set_color $fish_color_user) $USER (set_color normal)
+    set prompt_hostname '@' (set_color $fish_color_host_remote) $hostname (set_color normal)
   end
 
-  # Write pipestatus
+  set -l prompt_repo_name (string replace --regex --filter "$HOME/src/([^/]*).*" ' [$1]' $PWD; or true)
+
+  # Write pipestatus.
   set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
 
   echo -n -s \
     $prompt_user $prompt_hostname ' ' \
     (set_color $color_cwd) (prompt_pwd) (set_color normal) \
-    (prompt_repo) (fish_vcs_prompt) (set_color normal) \
+    $prompt_repo_name (fish_vcs_prompt) (set_color normal) \
     $prompt_status $suffix ' '
 end
