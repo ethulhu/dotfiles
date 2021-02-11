63d8c9870292fb4ea388fd1857d516d3e9542667
Ethel Morgan <eth@ethulhu.co.uk>
2021-02-11 17:14:38 +0000

    [fish] `make` wrapper doesn't change directory in a sub-shell
    it uses the `--directory=$dir` form instead.

--- .config/fish/functions/make.fish
+++ .config/fish/functions/make.fish
@@ -1,12 +1,14 @@
 set --local function_name (basename (status filename) .fish)
 
 function $function_name --wraps make --description 'Ascend the filesystem, looking for Makefiles.'
-    fish --private --command "
-    while ! [ -f 'Makefile' -o -f 'makefile' -o $PWD = $HOME -o $PWD = '/' ]
-      builtin cd ..
-    end
 
-    pwd 1>&2
-    command make $argv
-  "
+    set --local dir (fish --private --command "
+        while ! [ -f 'Makefile' -o -f 'makefile' -o $PWD = $HOME -o $PWD = '/' ]
+          builtin cd ..
+        end
+        pwd
+    ")
+
+    echo (status function)": $dir" 1>&2
+    command make --directory=$dir $argv
 end
