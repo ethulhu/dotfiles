2ebbedbcbfa114820ad0c0a22a019b8e4451cc50
Ethel Morgan <eth@ethulhu.co.uk>
2021-03-02 14:50:30 +0000

    [fish] implement my own `rm` confirmation dialog

---
+++ .config/fish/functions/rm.fish
@@ -0,0 +1,22 @@
+set --local function_name (basename (status filename) .fish)
+
+function $function_name --wraps rm
+
+    if not status is-interactive
+        command rm $argv
+    end
+
+    set --local items
+    for arg in $argv
+        if [ -e $arg ]
+            set --append items $arg
+        end
+    end
+
+    set --local num_dirs (find $items -type d | count)
+    set --local num_files (find $items -type f | count)
+
+    if confirm (status function)": delete $num_files files and $num_dirs dirs?"
+        command rm $argv
+    end
+end
