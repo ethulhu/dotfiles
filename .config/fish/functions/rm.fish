575d320dcc4b16807de68e5c98de9cb3ee0f7a9d
Ethel Morgan <eth@ethulhu.co.uk>
2021-03-05 16:46:14 +0000

    [fish] rm wrapper no-longer falls through to my code when non-interactive

--- .config/fish/functions/rm.fish
+++ .config/fish/functions/rm.fish
@@ -1,22 +1,25 @@
 set --local function_name (basename (status filename) .fish)
 
 function $function_name --wraps rm
-
     if not status is-interactive
         command rm $argv
-    end
-
-    set --local items
-    for arg in $argv
-        if [ -e $arg ]
-            set --append items $arg
+    else
+        set --local items
+        for arg in $argv
+            if [ -e $arg ]
+                set --append items $arg
+            end
         end
-    end
 
-    set --local num_dirs (find $items -type d | count)
-    set --local num_files (find $items -type f | count)
+        if [ (count $items) -eq 0 ]
+            command rm $argv
+        else
+            set --local num_dirs  (find $items -type d | count)
+            set --local num_files (find $items -type f | count)
 
-    if confirm (status function)": delete $num_files files and $num_dirs dirs?"
-        command rm $argv
+            if confirm (status function)": delete $num_files files and $num_dirs dirs?"
+                command rm $argv
+            end
+        end
     end
 end
