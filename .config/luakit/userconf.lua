0ffe5d6517e8a9371a763fc50556142828f4db97
Ethel Morgan <eth@ethulhu.co.uk>
2021-06-20 16:54:33 +0100

    [luakit] apply luakit settings in the config (vs the UI)

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -18,3 +18,15 @@ modes.add_binds("normal", {
         function() luakit.selection.clipboard = luakit.selection.primary end
     }
 })
+
+
+-- Misc settings.
+
+local settings = require "settings"
+
+settings.set_setting("session.always_save", true, {})
+settings.set_setting("webview.enable_accelerated_2d_canvas", true, {})
+settings.set_setting("window.default_search_engine", "duckduckgo", {})
+
+settings.set_setting("webview.javascript_can_access_clipboard", true,
+                     {domain = "moji.eth.moe"})
