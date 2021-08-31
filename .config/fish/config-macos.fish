2feebfff4f0c39d94929645a9dc51890037412b0
Ethel Morgan <eth@ethulhu.co.uk>
2021-08-31 10:08:30 +0100

    [macos] add plan9port binaries to PATH

--- .config/fish/config-macos.fish
+++ .config/fish/config-macos.fish
@@ -11,3 +11,9 @@ set --append fish_user_paths /Applications/OpenSCAD.app/Contents/MacOS
 set --local python_version (python3 --version | string match --regex '[[:digit:]]+\.[[:digit:]]+')
 set --export PYTHONPATH ~/.local/lib/python$python_version/site-packages
 set --prepend fish_user_paths ~/Library/Python/$python_version/bin
+
+set --export PLAN9 ~/src/plan9/plan9port
+
+# Append to PATH instead of fish_user_paths to put the plan9port binaries at
+# the absolute back of the queue.
+set --append PATH $PLAN9/bin
