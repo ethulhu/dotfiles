335076eeb048dd73cc3a558846af449ff35b0bea
Ethel Morgan <eth@ethulhu.co.uk>
2022-06-16 17:00:41 +0100

    [fish] do less (but still non-zero) manual opam setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,79 +21,18 @@ set --global --path fish_user_paths \
     $GOPATH/bin \
     ~/.cargo/bin
 
+# Set up OCaml.
+if [ -f ~/.opam/opam-init/init.fish ]
+    # source ~/.opam/opam-init/init.fish
 
-# Set up OCaml if `opam` has been added to the PATH.
-function __set_up_opam --on-variable PATH
-    if command --quiet opam; and command opam var prefix >/dev/null 2>/dev/null
-        if set --query __opam_set_up
-            return
-        end
-        set --global __opam_set_up true
-
-        if status is-interactive
-            # This stops `opam switch` telling us to reload the environment.
-            set --global --export OPAMNOENVNOTICE true
-        end
-
-        function __update_opam_env --on-event fish_preexec --on-event fish_postexec
-            if [ (command opam var prefix) != "$OPAM_SWITCH_PREFIX" ]
-                # Source everything except PATH.
-                command opam env --shell=fish --readonly \
-                    | string match --invert 'set -gx PATH *' \
-                    | source
-
-                # Trigger updating the PATH.
-                emit opam_switch_changed
-            end
-        end
-
-        function __update_opam_path --on-event opam_switch_changed
-            set --local opam_root (command opam var root)
-            set --local new_switch_bin (command opam var prefix)/bin
-
-            # Replace all paths matching $opam_root/*.
-            for path in $fish_user_paths
-                if string match --quiet -- "$opam_root/*" $path
-                    set fish_user_paths[(contains --index -- $path $PATH)] $new_switch_bin
-                end
-            end
-
-            # If there were no pre-existing $opam_root/* matches, prepend it.
-            if not contains -- $new_switch_bin $fish_user_paths
-                set --prepend fish_user_paths $new_switch_bin
-            end
-        end
-
-        # Erase any opam switches in PATH from fish's caller (e.g. tmux).
-        set --local opam_root (command opam var root)
-        for path in $PATH
-            if string match --quiet -- "$opam_root/*" $path
-                set --erase PATH[(contains --index -- $path $PATH)]
-            end
-        end
-
-        # Initial opam environment.
-        __update_opam_env
-        __update_opam_path
-
-        alias ocaml='rlwrap ocaml'
-        alias ml='ocaml'
-
-        function opam \
-                --wraps opam \
-                --description 'Unset OCaml-related environment variables before running opam'
-
-            # `env -u` isn't in POSIX, but is on macOS & Debian & NixOS.
-            # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/env.html
-            env \
-                -u DUNE_CACHE \
-                -u DUNE_PROFILE \
-                -u OCAMLRUNPARAM \
-                opam $argv
-        end
+    # opam's setup uses deprecated fish syntax, so it's copypasted here.
+    # TODO: remove ASAP.`
+    if isatty
+        cat ~/.opam/opam-init/env_hook.fish | string replace '^' '2>' | source
     end
+
+    source /Users/eth/.opam/opam-init/variables.fish > /dev/null 2> /dev/null; or true
 end
-__set_up_opam
 
 
 if command --quiet direnv
